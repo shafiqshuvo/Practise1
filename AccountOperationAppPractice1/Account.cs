70d48d682a37b70cb733f49c1f3ca580e3530155
shafiqshuvo <shafiq.shuvo.nstu.ice@gmail.com>
2019-10-16 11:34:54 +0600

    practise

---
+++ AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+   
+       public class Account
+       {
+            //private string accountNumber;
+            //private string customerName;
+
+            private double balance;
+
+            public string AccountNumber { set; get; }
+            public string CustomerName { set; get; }
+
+            public void Deposit(double amount)
+            {
+                if (amount > 0)
+                {
+                    balance += amount;
+                }
+                else
+                {
+                    MessageBox.Show("Positive amount");
+                }
+
+            }
+            public void Withdraw(double amount)
+            {
+                if (balance >= amount)
+                {
+                    balance -= amount;
+                }
+                else
+                {
+                    MessageBox.Show(@"No sufficient balance");
+                }
+
+            }
+
+            public string GetReport()
+            {
+                string message = CustomerName + ", your account no: " + AccountNumber + " and its balance:" + balance + " taka";
+                return message;
+            }
+       }
+    }
---
+++ AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+
+        Account account = new Account();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            if( !String.IsNullOrEmpty(accountNoTextBox.Text) && !String.IsNullOrEmpty(customerNameTextBox.Text))
+            {
+                account.AccountNumber = accountNoTextBox.Text;
+                account.CustomerName = customerNameTextBox.Text;
+
+                accountNoTextBox.Clear();
+                customerNameTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Feild Can't be empty!!!");
+            }
+            
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(amountTextBox.Text))
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                account.Deposit(amount);
+                amountTextBox.Clear();
+
+            }
+
+            else
+            {
+                MessageBox.Show("Feild Can't be empty!!!");
+            }
+
+
+
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(amountTextBox.Text))
+            {
+                double amount = Convert.ToDouble(amountTextBox.Text);
+                account.Withdraw(amount);
+
+                amountTextBox.Clear();
+            }
+
+            else
+            {
+                MessageBox.Show("Feild Can't be empty!!!");
+            }
+
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(account.GetReport());
+        }
+    }
+}
